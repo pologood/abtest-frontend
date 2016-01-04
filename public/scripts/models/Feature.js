52f78761adaeb0641d4c5f9f7fc63e91fb7d98cb
vitorpiassi <vitorpiassi@gmail.com>
2016-01-04 10:46:32 -0200

    AR-3046

--- public/scripts/models/Feature.js
+++ public/scripts/models/Feature.js
@@ -4,12 +4,8 @@ var Feature = Backbone.Model.extend({
 	urlRoot: app.backendUrl + '/features',
 	idAttribute: '_id',
 
-	getFeatures : function(){
-		return Backbone.sync("fetch", this, {async: false}).responseJSON;
-	},
-
-	enabling : function() {
-		this.toggleStatus(false);
+	enabling : function(status) {
+		this.save({enabled: status});
 	}
 
 });
@@ -18,8 +14,8 @@ var Features = Backbone.Collection.extend({
 	model: Feature,
 	url: '/features',
 
-	getFeature: function(feature) {
-		this.models[feature];
+	getFeatures: function() {
+		return Backbone.sync("fetch", this, {async: false}).responseJSON;
 	}
 });
 
