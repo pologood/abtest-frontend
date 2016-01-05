9c4f3dfbe79a9466e2d6050ec9df54238c8af5d6
vitorpiassi <vitorpiassi@gmail.com>
2016-01-05 18:01:40 -0200

    AR-3039

--- public/scripts/views/experiences/ExperienceVariations.js
+++ public/scripts/views/experiences/ExperienceVariations.js
@@ -5,54 +5,52 @@ class Variations extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
+		this.state.variations = [{hash: new Date().getTime()}];
 	}
 
 	render() {
+
+		var variations = this.state.variations;
+		var variationItems = [];
+
+		for (var i = 0, len = variations.length; i < len; i++) {
+			variationItems.push(
+			    <div key={variations[i].hash} className="form-group">
+			        <div className="col-xs-11">
+			            <input type="text" className="form-control" name="title" placeholder="Title" />
+			        </div>
+			        <div className="col-xs-1">
+			            <button type="button" className="btn btn-default removeButton" onClick={this._removeVariation.bind(this, i)}>-</button>
+			        </div>
+			    </div>
+			);
+		}
+
 		return (
 			<div>
 	      		<div className="page-header">
 					<h4 className="label-variation"><b>Variações</b></h4>
-					<button className="btn btn-success btn-sm btn-add-variation">
+					<button className="btn btn-success btn-sm btn-add-variation" onClick={this._addVariation.bind(this)}>
 						<span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
-						Adicionar
+						Adicionar mais
 					</button>
 				</div>
 
-				<div id="surveyForm" method="post" className="form-horizontal">
-				    <div className="form-group">
-				        <label className="col-xs-3 control-label">Question</label>
-				        <div className="col-xs-5">
-				            <input type="text" className="form-control" name="question" />
-				        </div>
-				    </div>
-
-				    <div className="form-group">
-				        <label className="col-xs-3 control-label">Options</label>
-				        <div className="col-xs-5">
-				            <input type="text" className="form-control" name="option[]" />
-				        </div>
-				        <div className="col-xs-4">
-				            <button type="button" className="btn btn-default addButton"><i className="fa fa-plus"></i></button>
-				        </div>
-				    </div>
-
-				    <div className="form-group hide" id="optionTemplate">
-				        <div className="col-xs-offset-3 col-xs-5">
-				            <input className="form-control" type="text" name="option[]" />
-				        </div>
-				        <div className="col-xs-4">
-				            <button type="button" className="btn btn-default removeButton"><i className="fa fa-minus"></i></button>
-				        </div>
-				    </div>
-
-				    <div className="form-group">
-				        <div className="col-xs-5 col-xs-offset-3">
-				            <button type="submit" className="btn btn-default">Validate</button>
-				        </div>
-				    </div>
-				</div>
+				{variationItems}
 			</div>
     	);
+	}
+
+	_addVariation(event) {
+		event.preventDefault();
+		this.state.variations.push({hash: new Date().getTime()});
+		this.setState({variations: this.state.variations});
+	}
+
+	_removeVariation(itemId) {
+		debugger;
+		this.state.variations.splice(itemId, 1);
+		this.setState({variations: this.state.variations});
 	}
 }
 
